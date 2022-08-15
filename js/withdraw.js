0016c71a2c03f80f9b1e35ac21d2f7e9969e29df
Fnowshin <fnowshin5@gmail.com>
2022-08-16 04:36:21 +0600

    finally

--- js/withdraw.js
+++ js/withdraw.js
@@ -12,20 +12,29 @@ const withdrawTotalElement = document.getElementById('withdraw-total');
 const previousWithdrawTotalString = withdrawTotalElement.innerText;
 const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-// 4. calculate total witdraw amount
-const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+// // 4. calculate total witdraw amount
+// const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
 
-// 4.5. set total withdraw amount
-withdrawTotalElement.innerText = currentWithdrawTotal;
+// // 4.5. set total withdraw amount
+// withdrawTotalElement.innerText = currentWithdrawTotal;
 
 // 5. get the previous balance total
 const balanceTotalElement = document.getElementById('balance-total');
 const previousBalanceTotalString = balanceTotalElement.innerText;
 const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+// 7. clear the withdraw box
+withdrawField.value = ' ';
+
+if(newWithdrawAmount > previousBalanceTotal){
+    alert('Not enough money');
+    return;
+}
+const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+withdrawTotalElement.innerText = currentWithdrawTotal;
+
 // 6. get the new balance after withdrawing
 const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 balanceTotalElement.innerText = newBalanceTotal;
-// 7. clear the withdraw box
-withdrawField.value = ' ';
+
 })
