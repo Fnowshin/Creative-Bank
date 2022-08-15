d6184a5a8d1e1576e5bae91947251abc0ce8dcd5
Fnowshin <fnowshin5@gmail.com>
2022-08-16 03:35:01 +0600

    deposit

--- js/deposit.js
+++ js/deposit.js
@@ -1,10 +1,15 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
    const depositField = document.getElementById('deposit-field')
-   const depositAmount = depositField.value;
+   const newDepositAmountString = depositField.value;
+   const newDepositAmount = parseFloat(newDepositAmountString);
    
    const depositTotalElement = document.getElementById('deposit-total');
-   const depositTotal = depositTotalElement.innerText;
-   depositTotalElement.innerText = depositAmount;
+   const previousDepositTotalString = depositTotalElement.innerText;
+   const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+   const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+   depositTotalElement.innerText = currentDepositTotal;
 
    depositField.value = ' ';
 })
